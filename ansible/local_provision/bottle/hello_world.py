94747476e5186222e3b5a4828df9b4b8ecc82943
hori-esw <hori@e-sw.co.jp>
2015-05-12 00:26:40 +0900

    sqliteのインストールを含めた　アプリを更新した

--- ansible/local_provision/bottle/hello_world.py
+++ ansible/local_provision/bottle/hello_world.py
@@ -1,7 +1,8 @@
 __author__ = 'hori'
+# -*- coding: utf-8 -*-
 
 # モジュールのimport
-from bottle import route, run, template
+from bottle import route, get, post, run, template, request
 
 # ルーティングの設定
 @route('/hello')
@@ -20,6 +21,27 @@ def greeting(name):
     #     name = 'world'
 
     return template('Hello {{name}}.', name=name)
+
+@route('/show_header')
+def show_header():
+    #ヘッダ情報の表示
+    headers_list = ["<p> %s = %s </p>" % (k, v) for k, v in request.headers.items()]
+    return "".join(headers_list)
+
+@route('/show_cookie')
+def show_cookie():
+    count = request.cookies.get('count')
+    return template('count={{count}}', count=count)
+
+@get('/show_query')
+def show_query():
+    keyword = request.query.keyword
+    return template('keyword={{keyword}}', keyword=keyword)
+
+@post('/show_form')
+def show_form():
+    name = request.forms.get('name')
+    return template('name={{name}}', name=name)
 
 # ビルトイン開発用サーバの起動
 if __name__ == '__main__':
